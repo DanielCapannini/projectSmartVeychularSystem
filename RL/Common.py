1289365d96bc8b9d234c05609f0f80619cd92ff7
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-11-27 13:57:12 +0100

    .

--- RL/Common.py
+++ RL/Common.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-
+import carla
 
 def preprocess_image(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -16,3 +16,17 @@ def preprocess_image(image):
     image_opened = cv2.morphologyEx(image_closed, cv2.MORPH_OPEN, (5,5))
     image_opened[image_opened > 0] = 255
     return image_opened
+
+def spawn_vehicle(world, vehicle_index=0, spawn_index=0, pattern='vehicle.*'):
+    blueprint_library = world.get_blueprint_library()
+    vehicle_bp = blueprint_library.filter(pattern)[vehicle_index]
+    spawn_point = world.get_map().get_spawn_points()[spawn_index]
+    vehicle = world.spawn_actor(vehicle_bp, spawn_point)
+    return vehicle
+
+def spawn_camera(world, attach_to=None, transform=carla.Transform(carla.Location(x=1.2, z=1.2), carla.Rotation(pitch=-10)), width=800, height=600):
+    camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
+    camera_bp.set_attribute('image_size_x', str(width))
+    camera_bp.set_attribute('image_size_y', str(height))
+    camera = world.spawn_actor(camera_bp, transform, attach_to=attach_to)
+    return camera
