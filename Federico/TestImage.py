828700341af497673c0e88abf90ad4acdd542790
federicobonetti99 <federico.bonetti2@studio.unibo.it>
2024-11-27 16:26:11 +0100

    messa a posto la cartella Federico

--- Federico/TestImage.py
+++ Federico/TestImage.py
@@ -1,8 +1,29 @@
 import cv2
-import os
+import numpy as np
+import time
 import tkinter as tk
 
-root = tk.Tk()
-immagine_tk = tk.PhotoImage(file='img/parcheggio.jpg')
-tk.Label(root, image=immagine_tk).pack()
-root.mainloop()
+
+def processImage(imageURL):
+    image = cv2.imread(imageURL)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresholded = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    white_pixels = np.where(thresholded == 255)
+    sorted_pixels = np.sort(gray_image[white_pixels])
+    threshold_value = sorted_pixels[int(0.85 * len(sorted_pixels))]
+    _, custom_thresholded = cv2.threshold(gray_image, threshold_value, 255, cv2.THRESH_BINARY)
+    mask = np.zeros_like(gray_image)
+    mask[custom_thresholded == 255] = gray_image[custom_thresholded == 255]
+    kernel = np.ones((3, 3), np.uint8)
+    image_closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    image_opened = cv2.morphologyEx(image_closed, cv2.MORPH_OPEN, (5,5))
+    image_opened[image_opened > 0] = 255
+    cv2.imshow("Result Image", image_opened)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+imageURL = "./Federico/img/parcheggio.jpg"
+processImage(imageURL)
+
+import os
+print("Directory corrente:", os.getcwd())
