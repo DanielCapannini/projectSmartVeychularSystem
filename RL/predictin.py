2e5c552f88b3bd59ffcbdbfe763e43ab30ea3f16
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-11-29 16:28:46 +0100

    Update predictin.py

--- RL/predictin.py
+++ RL/predictin.py
@@ -5,16 +5,18 @@ from stable_baselines3.common.buffers import ReplayBuffer
 
 from RL.CarlaEnv import CarlaEnv
 
-data = np.load("expert_data.npz")
-observations = data["observations"]
-actions = data["actions"]
 
-env = make_vec_env(CarlaEnv, n_envs=1)
-model = PPO("CnnPolicy", env, verbose=1)
+def predict():
+    data = np.load("expert_data.npz")
+    observations = data["observations"]
+    actions = data["actions"]
 
-buffer = ReplayBuffer(len(observations), env.observation_space, env.action_space)
-for obs, action in zip(observations, actions):
-    buffer.add(obs, action, reward=0, done=False, infos={})
+    env = make_vec_env(CarlaEnv, n_envs=1)
+    model = PPO("CnnPolicy", env, verbose=1)
 
-model.train_on_dataset(buffer, n_epochs=10)
-model.save("ppo_pretrained")
+    buffer = ReplayBuffer(len(observations), env.observation_space, env.action_space)
+    for obs, action in zip(observations, actions):
+        buffer.add(obs, action, reward=0, done=False, infos={})
+
+    model.train_on_dataset(buffer, n_epochs=10)
+    model.save("ppo_pretrained")
