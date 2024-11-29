daf09fe5834ae9a3ff2ad21811863802b52b1430
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-11-29 16:27:08 +0100

    Revert "cambio cartella"
    This reverts commit 15b7b14aad2c1f39ef318d5db521328b7acf9a80.

--- RL/a.py
+++ RL/a.py
@@ -2,7 +2,7 @@ import carla, time, pygame, math, random, cv2
 import numpy as np
 
 client = carla.Client('localhost', 2000)
-client.set_timeout(20.0)
-client.load_world('Town01')
+client.set_timeout(10.0)
+client.load_world('Town5')
 world = client.get_world()
 spectator = world.get_spectator()
