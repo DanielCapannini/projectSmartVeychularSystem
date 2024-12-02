6444cfb706d5959d896e07cdf5ec0d7db79cc613
Daniel Capannini <daniel.capannini@studio.unibo.it>
2024-12-02 15:30:49 +0100

    nuovo carlaEnv

--- RL/CarlaEnv.py
+++ RL/CarlaEnv.py
@@ -4,12 +4,12 @@ import numpy as np
 import carla
 import random
 
-from RL.Common import spawn_camera, preprocess_image, spawn_vehicle
+from Common import spawn_camera, preprocess_image, spawn_vehicle
 
 camera_transforms = [
-    (carla.Transform(carla.Location(x=1.5, z=2.4)), (600, 300)),  # Front camera
-    (carla.Transform(carla.Location(x=-0.5, y=-0.9, z=2.4), carla.Rotation(yaw=-135)), (200, 400)),  # Left side camera
-    (carla.Transform(carla.Location(x=-0.5, y=0.9, z=2.4), carla.Rotation(yaw=135)), (200, 400)),  # Right side camera
+    (carla.Transform(carla.Location(x=1.5, z=2.4), carla.Rotation(pitch=-25)), (600, 300)),  # Front camera
+    (carla.Transform(carla.Location(x=-0.5, y=-0.9, z=2.4), carla.Rotation(yaw=-135, pitch=-25)), (600, 300)),  # Left side camera
+    (carla.Transform(carla.Location(x=-0.5, y=0.9, z=2.4), carla.Rotation(yaw=135, pitch=-25)), (600, 300)),  # Right side camera
 ]
 
 class CarlaEnv(gym.Env):
@@ -17,57 +17,56 @@ class CarlaEnv(gym.Env):
         super(CarlaEnv, self).__init__()
 
         self.client = carla.Client('localhost', 2000)
-        self.client.set_timeout(10.0)
-        self.client.load_world('Town5')
+        self.client.set_timeout(20.0)
+        self.client.load_world('Town05')
         self.world = self.client.get_world()
 
+        self.observation_space = spaces.Box(low=0, high=255, shape=(3, 300, 600, 3), dtype=np.uint8)  # Shape is (3 cameras, 300x600 px, 3 color channels)
+        self.action_space = spaces.Discrete(4)  # 4 actions: accelerate, brake, steer left, steer right
 
-        self.observation_space = spaces.Box(low=0, high=255, shape=(640, 480, 3), dtype=np.uint8)
-        self.action_space = spaces.Discrete(4)  # 4 azioni: accelerare, frenare, sterzare sinistra, sterzare destra
-
-        # Creazione di un veicolo (simulazione)
         self.vehicle = None
-        self.image = None
+        self.camera = []
+        self.image1 = np.zeros((300, 600))
+        self.image2 = np.zeros((300, 600))
+        self.image0 = np.zeros((300, 600))
+        self.counter = 0
 
     def reset(self):
-        # Resetta l'ambiente e il veicolo
+        # Reset the environment, spawn a new vehicle, and attach cameras
         self.vehicle = self._spawn_vehicle()
         self.camera = self._attach_camera(self.vehicle)
 
-        # Ritorna lo stato iniziale (in questo caso l'immagine dalla telecamera)
+        # Return the initial state (images from the cameras)
         state = self._get_observation()
+        self.counter = 0
         return state
 
-    def step(self, action):
-        # Esegui l'azione selezionata
-        if action == 0:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))  # Accelerazione
-        elif action == 1:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=1.0))  # Frenata
-        elif action == 2:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, steer=-1.0))  # Sterzare a sinistra
-        elif action == 3:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, steer=1.0))  # Sterzare a destra
-
-        # Ottieni il nuovo stato (l'immagine della telecamera)
+    def step(self, throttle, steer, brake):
+        self.vehicle.apply_control(carla.VehicleControl(throttle=throttle, steer=steer, brake=brake))
         state = self._get_observation()
-
-        # Calcola la ricompensa (questa è una semplice ricompensa casuale)
-        reward = random.random()#da implementare
-
-        # Termina l'episodio (se il veicolo esce dalla strada, per esempio)
-        done = random.random() > 0.95  # Termina con probabilità 5%
-
+        reward = self.render()
+        done = self.done()
         return state, reward, done, {}
+    
+    def reward(self):
+        return 0
+    
+    
+    def done(self):
+        self.counter += 1
+        if self.counter >= 3000:
+            self.vehicle.destroy()
+            return True
+        else:
+            return False
 
     def render(self, mode='human'):
-        # Visualizza l'immagine dalla telecamera (opzionale, può essere utile per il debug)
         pass
 
     def close(self):
-        # Chiudi la connessione con Carla
         if self.camera:
-            self.camera.stop()
+            for cam in self.camera:
+                cam.stop()
         if self.vehicle:
             self.vehicle.destroy()
 
@@ -75,27 +74,38 @@ class CarlaEnv(gym.Env):
         return spawn_vehicle(self.world)
 
     def _attach_camera(self, vehicle):
-        camera_list = np.array([])
+        camera_list = []
         for i, transform in enumerate(camera_transforms):
             camera = spawn_camera(self.world, attach_to=vehicle, transform=transform[0], width=transform[1][0], height=transform[1][1])
-            camera.listen(lambda image: self._process_image(image, i))
-            camera_list = np.append(camera_list, camera)
+            camera.listen(lambda image, idx=i: self._process_image(image, idx))
+            camera_list.append(camera)
         return camera_list
 
     def _process_image(self, image, index):
-        self.image[index] = np.array(preprocess_image(image))
+        image_output_left = np.reshape(np.copy(image.raw_data), (image.height, image.width, 4))
+        processed_image = np.array(preprocess_image(image_output_left))
+        if index==0:
+            self.image0 = processed_image
+        if index==1:
+            self.image1 = processed_image
+        if index==2:
+            self.image2 = processed_image
 
     def _get_observation(self):
-        return self.image
+        return np.stack((self.image0, self.image1, self.image2), axis=0)
 
-    def collect_expert_data(env, esperto, num_episodi=10, file_path="expert_data.npz"):
+    @staticmethod
+    def collect_expert_data(env, expert, num_episodes=10, file_path="./expert_data.npz"):
         data = {"observations": [], "actions": []}
-        for _ in range(num_episodi):
+        print("start epoch")
+        for i in range(num_episodes):
+            print("start "+ str(i)+ " epoch")
             obs = env.reset()
             done = False
             while not done:
-                action = esperto(obs)  # L'azione definita dall'esperto
+                throttle, steer, brake = expert(obs)
                 data["observations"].append(obs)
-                data["actions"].append(action)
-                obs, _, done, _ = env.step(action)
+                data["actions"].append((throttle, steer, brake))
+                obs, _, done, _ = env.step(throttle, steer, brake)
+            print("end "+ str(i)+ " epoch")
         np.savez(file_path, **data)
