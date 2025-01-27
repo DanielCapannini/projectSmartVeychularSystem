eb51b67001ac02a4e267fe6b93342eb3877a57a2
federicobonetti99 <federico.bonetti2@studio.unibo.it>
2025-01-27 10:10:10 +0100

    removed useless comment

--- test/ParkingIdentifierTest/TestImageFinal2.py
+++ test/ParkingIdentifierTest/TestImageFinal2.py
@@ -6,7 +6,6 @@ import uuid
 import math
 from typing import List, Tuple, Optional
 
-# Funzione per migliorare l'immagine (equalizzazione dell'istogramma)
 def preprocess_image(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, binary_mask = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
@@ -24,38 +23,29 @@ def find_and_draw_longest_white_line(binary_image):
     Returns:
         numpy.ndarray: Immagine con la linea più lunga disegnata.
     """
-
-    # Crea una copia dell'immagine per disegnare la linea
     image_with_line = cv2.cvtColor(binary_image.copy(), cv2.COLOR_GRAY2BGR)
-
-    # Trova i contorni nell'immagine
     contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    longest_line = None  # La linea più lunga trovata
+    longest_line = None 
     max_length = 0
 
     for contour in contours:
-        # Approssima il contorno con segmenti di linea
         approx = cv2.approxPolyDP(contour, epsilon=1, closed=False)
 
         for i in range(len(approx) - 1):
-            # Calcola la lunghezza del segmento di linea
             x1, y1 = approx[i][0]
             x2, y2 = approx[i + 1][0]
             length = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
             lengthX = abs(x2-x1)
             lengthY = abs(y2-y1)
             
-            #print(f"Lenght: {length}, LenghtX: {lengthX}")
-
-            if lengthX > lengthY*2: #linea orizzontale
+            if lengthX > lengthY*2: 
                 if lengthX > max_length:
                     max_length = lengthX
                     #print(f"max_LenghtX: {max_length}")
                     longest_line = ((x1, y1), (x2, y2))
                     #cv2.line(image_with_line, longest_line[0], longest_line[1], (0, 0, 255), 2)  # Linea rossa di debug
 
-    # Disegna la linea più lunga trovata
     if longest_line is not None:
         #print(f"Scelto: LenghtX: {max_length}")
         cv2.line(image_with_line, longest_line[0], longest_line[1], (0, 0, 255), 2)  # Linea rossa
@@ -135,37 +125,28 @@ def color_enclosed_black_areas(image, color=(0, 255, 0), min_area=500, epsilon_f
             - Booleano che indica se è stato trovato almeno un poligono con 4 lati.
             - Lista di liste dei vertici dei poligoni trovati.
     """
-    # Crea una copia dell'immagine per non modificare l'originale
-    colored_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)  # Converti l'immagine in un'immagine a colori
-    found_polygons = []  # Per memorizzare i vertici dei poligoni trovati
+    colored_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    found_polygons = []
 
-    # Trova i contorni nell'immagine binaria (invertiamo l'immagine per cercare il nero)
     contours, _ = cv2.findContours(255 - image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Ottieni le dimensioni dell'immagine
     height, width = image.shape
     center = None
-    # Filtra i contorni per area minima e quelli che non toccano i bordi
     for contour in contours:
         area = cv2.contourArea(contour)
-        x, y, w, h = cv2.boundingRect(contour)  # Ottieni il rettangolo del contorno
+        x, y, w, h = cv2.boundingRect(contour)
 
-        # Controlla che il contorno non tocchi i bordi
         if x > 0 and y > 0 and (x + w) < width and (y + h) < height:
-            if area > min_area:  # Verifica se l'area del contorno è maggiore della soglia
-                # Riempi il contorno con il colore specificato
+            if area > min_area:
                 cv2.drawContours(colored_image, [contour], -1, color, thickness=cv2.FILLED)
                 
-                # Approssima il contorno a un poligono
                 epsilon = epsilon_factor * cv2.arcLength(contour, True)
                 approx = cv2.approxPolyDP(contour, epsilon, True)
                 
-                # Se il poligono ha 4 lati, memorizza i suoi angoli
                 if len(approx) == 4:
                     corners = [(point[0][0], point[0][1]) for point in approx]
                     found_polygons.append(corners)
                     
-                    # Disegna i vertici sull'immagine
                     for corner in corners:
                         cv2.circle(colored_image, corner, radius=10, color=(0, 0, 255), thickness=-1)  # Cerchio rosso
                         
@@ -173,27 +154,17 @@ def color_enclosed_black_areas(image, color=(0, 255, 0), min_area=500, epsilon_f
                     center = (int(center[0]), int(center[1]))
                     cv2.circle(colored_image, center, radius=10, color=(0, 255, 255), thickness=-1)
 
-    # Ritorna l'immagine, il booleano e il centro trovato
                     center = (int(center[0]) - (width/2), int(center[1]))
-    #se vanno specchiate le coordinate
-    # center = (-int(center[0]), int(center[1]))
+
     return colored_image, len(found_polygons) > 0, center
 
 def find_center_of_polygon(corners):
-    # Assumiamo che `corners` sia una lista di tuple con 4 punti, ad esempio [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
-    
-    # Calcola la media delle coordinate x e y
     center_x = sum([corner[0] for corner in corners]) / len(corners)
     center_y = sum([corner[1] for corner in corners]) / len(corners)
     
     return (center_x, center_y)
 
 def process_image(imageURL, i=0):
-    # Assicurati che la cartella 'output' esista
-    #output_dir = "./Federico/project/output"
-    #os.makedirs(output_dir, exist_ok=True)  # Crea la cartella se non esiste
-
-    # Preprocessing dell'immagine
     image_opened = preprocess_image(imageURL)  # Pre-processing dell'immagine
     angle_radians = (find_and_draw_longest_white_line(image_opened))
     print(f"Angolo trovato: {angle_radians}")
